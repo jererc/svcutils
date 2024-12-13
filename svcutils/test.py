7e57cad1d3ab2aab15b98bf27f9f2bae5719cb05
jererc <jererc@gmail.com>
2024-12-13 17:33:47 +0400

    do epic things

--- svcutils/test.py
+++ svcutils/test.py
@@ -1,8 +1,28 @@
 import os
+from pathlib import Path
 import sys
 
-print(f'{os.getcwd()=}')
-print(f'{__file__=}')
-print(f'{sys.stdin.isatty()=}')
-print(f'{sys.stdout.isatty()=}')
-print(f'{sys.argv=}')
+def is_relative_to(base_path, target_path):
+    base = Path(base_path).resolve()
+    target = Path(target_path).resolve()
+    return target.is_relative_to(base)
+
+
+def get_valid_cwd():
+    path = os.getcwd()
+    admin_dir = {
+        'nt': os.environ.get('WINDIR', r'C:\Windows'),
+        'posix': os.environ.get('WINDIR', r'/root'),
+    }[os.name]
+    if is_relative_to(admin_dir, path):
+        raise ValueError(f'invalid current working dir {path}')
+    return path
+
+
+# print(f'{os.getcwd()=}')
+# print(f'{__file__=}')
+# print(f'{sys.stdin.isatty()=}')
+# print(f'{sys.stdout.isatty()=}')
+# print(f'{sys.argv=}')
+
+print(f'{get_valid_cwd()=}')
