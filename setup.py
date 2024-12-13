ff1f65c68227dc36258457aeabbc2b8bfd8cda98
jererc <jererc@gmail.com>
2024-12-13 16:31:41 +0400

    do epic things

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='svcutils',
-    version='2024.12.13.162019',
+    version='2024.12.13.163141',
     author='jererc',
     author_email='jererc@gmail.com',
     url='https://github.com/jererc/svcutils',
--- svcutils/test.py
+++ svcutils/test.py
@@ -3,4 +3,5 @@ import sys
 
 print(f'{os.getcwd()=}')
 print(f'{__file__=}')
+print(f'{sys.stdin.isatty()=}')
 print(f'{sys.argv=}')
