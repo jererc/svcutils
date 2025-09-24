bb6411abb128b1b34b03fe3d1777d4d9b2f0459f
jererc <jererc@gmail.com>
2025-09-24 18:19:28 +0400

    do epic things

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='svcutils',
-    version='2025.09.24.181727',
+    version='2025.09.24.181927',
     author='jererc',
     author_email='jererc@gmail.com',
     url='https://github.com/jererc/svcutils',
