37131a37b86a00e9a06ddec1cfd88e4e671f5d76
jererc <jererc@gmail.com>
2026-01-17 18:00:48 +0400

    do epic things

--- svcutils/bootstrap.py
+++ svcutils/bootstrap.py
@@ -39,19 +39,16 @@ def get_work_dir(name):
 
 
 class Bootstrapper:
-    def __init__(self, name, cmd_args=None, cmd_terminal=False,
-                 install_requires=None, force_reinstall=False,
-                 init_cmds=None, extra_cmds=None, download_assets=None,
-                 schedule_minutes=2):
+    def __init__(self, name, install_requires=None, force_reinstall=False, init_cmds=None, extra_cmds=None,
+                 tasks=None, shortcuts=None, download_assets=None):
         self.name = name
-        self.cmd_args = cmd_args
-        self.cmd_terminal = cmd_terminal
         self.install_requires = install_requires
         self.force_reinstall = force_reinstall
         self.init_cmds = init_cmds
         self.extra_cmds = extra_cmds
-        self.schedule_minutes = schedule_minutes
-        self.download_assets = download_assets
+        self.tasks = tasks or []
+        self.shortcuts = shortcuts or []
+        self.download_assets = download_assets or []
         self.cwd = get_valid_cwd()
         self.work_dir = get_work_dir(self.name)
         self.venv_dir = os.path.join(self.work_dir, VENV_DIRNAME)
@@ -59,6 +56,7 @@ class Bootstrapper:
         self.pip_path = os.path.join(self.venv_bin_dir, VENV_PIP_PATH)
         self.py_path = os.path.join(self.venv_bin_dir, VENV_PY_PATH)
         self.svc_py_path = os.path.join(self.venv_bin_dir, VENV_SVC_PY_PATH)
+        self._setup()
 
     def _run_venv_cmds(self, cmds):
         for cmd in cmds:
@@ -66,7 +64,7 @@ class Bootstrapper:
             print(f'running: {" ".join(venv_cmd)}')
             subprocess.check_call(venv_cmd)
 
-    def setup_venv(self):
+    def _setup_venv(self):
         requires_init = not os.path.exists(self.pip_path)
         if requires_init:
             subprocess.check_call([sys.executable, '-m', 'venv', self.venv_dir])   # requires python3-venv
@@ -82,39 +80,80 @@ class Bootstrapper:
             self._run_venv_cmds(self.extra_cmds)
 
     def _download_assets(self):
-        if not self.download_assets:
-            return
         for filename, url in self.download_assets:
             file = os.path.join(self.cwd, filename)
             if not os.path.exists(file):
                 urllib.request.urlretrieve(url, file)
                 print(f'created asset: {file}')
 
-    def _get_cmd(self):
-        if not self.cmd_args:
-            raise SystemExit('Error: missing cmd_args')
-        py_path = self.py_path if self.cmd_terminal else self.svc_py_path
-        return [py_path, '-m'] + self.cmd_args
+    def _create_windows_shortcut(self, target_path, shortcut_path, arguments='', working_dir='', description=''):
+        vbs_content = f"""Set objShell = WScript.CreateObject("WScript.Shell")
+Set objShortcut = objShell.CreateShortcut("{shortcut_path}")
+objShortcut.TargetPath = "{target_path}"
+objShortcut.Arguments = "{arguments}"
+objShortcut.WorkingDirectory = "{working_dir or os.path.dirname(target_path)}"
+objShortcut.Description = "{description}"
+objShortcut.Save
+"""
+        temp_file = os.path.join(self.cwd, 'temp.vbs')
+        with open(temp_file, 'w') as fd:
+            fd.write(vbs_content)
+        try:
+            os.system(f'cscript //NoLogo "{temp_file}"')
+        finally:
+            os.remove(temp_file)
 
-    def _generate_crontab_schedule(self):
-        match self.schedule_minutes:
-            case _ if self.schedule_minutes < 2:
+    def _create_linux_shortcut(self, name, cmd, shortcut_path, description=''):
+        os.makedirs(os.path.dirname(shortcut_path), exist_ok=True)
+        content = f"""[Desktop Entry]
+Type=Application
+Name={name}
+Exec={cmd}
+Terminal=true
+Comment={description}
+"""
+        with open(shortcut_path, 'w') as fd:
+            fd.write(content)
+        subprocess.check_call(['chmod', '+x', shortcut_path])
+
+    def _setup_shortcut(self, name, args, headless=True):
+        py_path = self.svc_py_path if headless else self.py_path
+        args_str = f'-m {" ".join(args)}'
+        if sys.platform == 'win32':
+            file = os.path.join(APP_DIR, f'{name}.lnk')
+            self._create_windows_shortcut(
+                target_path=py_path,
+                shortcut_path=file,
+                arguments=args_str,
+                working_dir=self.work_dir,
+                description=name,
+            )
+        else:
+            file = os.path.join(APP_DIR, f'{name}.desktop')
+            self._create_linux_shortcut(
+                name=name,
+                cmd=f'{py_path} {args_str}',
+                shortcut_path=file,
+                description=name,
+            )
+        print(f'created shortcut: {file}')
+
+    def _generate_crontab_schedule(self, schedule_minutes):
+        match schedule_minutes:
+            case _ if schedule_minutes < 2:
                 return '* * * * *'
-            case _ if self.schedule_minutes < 60:
-                return f'*/{self.schedule_minutes} * * * *'
-            case _ if self.schedule_minutes < 60 * 24:
-                hours = self.schedule_minutes // 60
-                if hours == 1:
-                    return '0 * * * *'
-                return f'0 */{hours} * * *'
+            case _ if schedule_minutes < 60:
+                return f'*/{schedule_minutes} * * * *'
+            case _ if schedule_minutes < 60 * 24:
+                hours = schedule_minutes // 60
+                return '0 * * * *' if hours == 1 else f'0 */{hours} * * *'
             case _:
                 return '0 0 * * *'
 
-    def _setup_linux_crontab(self, cmd):
-        res = subprocess.run(['crontab', '-l'], stdout=subprocess.PIPE,
-                             stderr=subprocess.PIPE, text=True)
+    def _setup_linux_crontab(self, cmd, name, schedule_minutes):
+        res = subprocess.run(['crontab', '-l'], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
         current_crontab = res.stdout if res.returncode == 0 else ''
-        new_job = f'{self._generate_crontab_schedule()} flock -n /tmp/{self.name}.lock {cmd}\n'
+        new_job = f'{self._generate_crontab_schedule(schedule_minutes)} flock -n /tmp/{name}.lock {cmd}\n'
         updated_crontab = ''
         job_found = False
         for line in current_crontab.splitlines():
@@ -130,7 +169,7 @@ class Bootstrapper:
             raise SystemExit('Error: failed to update crontab')
         print(f'created crontab job:\n{new_job.strip()}')
 
-    def _setup_windows_task(self, cmd, task_name):
+    def _setup_windows_task(self, cmd, task_name, schedule_minutes):
         if ctypes.windll.shell32.IsUserAnAdmin() == 0:
             raise SystemExit('Error: must run as admin to update scheduled tasks')
         subprocess.check_call([
@@ -138,95 +177,56 @@ class Bootstrapper:
             '/tn', task_name,
             '/tr', cmd,
             '/sc', 'minute',
-            '/mo', str(self.schedule_minutes),
+            '/mo', str(schedule_minutes),
             '/rl', 'highest',
             '/f',
         ])
         print(f'created scheduled task {task_name} with cmd:\n{cmd}')
 
-    def _create_linux_shortcut(self, name, cmd, shortcut_path,
-                               description=''):
-        os.makedirs(os.path.dirname(shortcut_path), exist_ok=True)
-        content = f"""[Desktop Entry]
-Type=Application
-Name={name}
-Exec={cmd}
-Terminal=true
-Comment={description}
-"""
-        with open(shortcut_path, 'w') as fd:
-            fd.write(content)
-        subprocess.check_call(['chmod', '+x', shortcut_path])
+    def _setup_task(self, name, args, schedule_minutes=2):
+        cmd = ' '.join([self.svc_py_path, '-m'] + args)
+        if sys.platform == 'win32':
+            self._setup_windows_task(cmd=cmd, task_name=name, schedule_minutes=schedule_minutes)
+        else:
+            self._setup_linux_crontab(cmd=cmd, name=name, schedule_minutes=schedule_minutes)
 
-    def _create_windows_shortcut(self, target_path, shortcut_path,
-                                 arguments='', working_dir='', description=''):
-        if not working_dir:
-            working_dir = os.path.dirname(target_path)
-        vbs_content = f"""Set objShell = WScript.CreateObject("WScript.Shell")
-Set objShortcut = objShell.CreateShortcut("{shortcut_path}")
-objShortcut.TargetPath = "{target_path}"
-objShortcut.Arguments = "{arguments}"
-objShortcut.WorkingDirectory = "{working_dir}"
-objShortcut.Description = "{description}"
-objShortcut.Save
-"""
-        temp_file = os.path.join(self.cwd, 'temp.vbs')
-        with open(temp_file, 'w') as fd:
-            fd.write(vbs_content)
-        try:
-            os.system(f'cscript //NoLogo "{temp_file}"')
-        finally:
-            os.remove(temp_file)
-
-    def setup_shortcut(self):
-        self.setup_venv()
+    def _setup(self):
+        self._setup_venv()
         self._download_assets()
-        cmd = self._get_cmd()
-        if sys.platform == 'win32':
-            file = os.path.join(APP_DIR, f'{self.name}.lnk')
-            self._create_windows_shortcut(
-                target_path=cmd[0],
-                shortcut_path=file,
-                arguments=' '.join(cmd[1:]),
-                working_dir=self.cwd,
-                description=self.name,
-            )
-        else:
-            file = os.path.join(APP_DIR, f'{self.name}.desktop')
-            self._create_linux_shortcut(
-                name=self.name,
-                cmd=' '.join(cmd),
-                shortcut_path=file,
-                description=self.name,
-            )
-        print(f'created shortcut: {file}')
+        for kwargs in self.shortcuts:
+            self._setup_shortcut(**kwargs)
+        for kwargs in self.tasks:
+            self._setup_task(**kwargs)
 
-    def setup_script_shortcut(self, name, args):
-        args_str = f'-m {" ".join(args)}'
-        if sys.platform == 'win32':
-            file = os.path.join(APP_DIR, f'{name}.lnk')
-            self._create_windows_shortcut(
-                target_path=self.py_path,
-                shortcut_path=file,
-                arguments=args_str,
-                working_dir=self.cwd,
-                description=name,
-            )
-        else:
-            file = os.path.join(APP_DIR, f'{name}.desktop')
-            self._create_linux_shortcut(
-                name=name,
-                cmd=f'{self.py_path} {args_str}',
-                shortcut_path=file,
-                description=name,
-            )
-        print(f'created shortcut: {file}')
+    # def _get_cmd(self):
+    #     if not self.cmd_args:
+    #         raise SystemExit('Error: missing cmd_args')
+    #     return [self.svc_py_path, '-m'] + self.cmd_args
 
-    def setup_task(self):
-        self.setup_venv()
-        self._download_assets()
-        cmd = ' '.join(self._get_cmd())
-        if sys.platform == 'win32':
-            self._setup_windows_task(cmd=cmd, task_name=self.name)
-        else:
-            self._setup_linux_crontab(cmd=cmd)
+    # def setup_shortcut(self):
+    #     cmd = self._get_cmd()
+    #     if sys.platform == 'win32':
+    #         file = os.path.join(APP_DIR, f'{self.name}.lnk')
+    #         self._create_windows_shortcut(
+    #             target_path=cmd[0],
+    #             shortcut_path=file,
+    #             arguments=' '.join(cmd[1:]),
+    #             working_dir=self.cwd,
+    #             description=self.name,
+    #         )
+    #     else:
+    #         file = os.path.join(APP_DIR, f'{self.name}.desktop')
+    #         self._create_linux_shortcut(
+    #             name=self.name,
+    #             cmd=' '.join(cmd),
+    #             shortcut_path=file,
+    #             description=self.name,
+    #         )
+    #     print(f'created shortcut: {file}')
+
+    # def setup_task(self):
+    #     cmd = ' '.join(self._get_cmd())
+    #     if sys.platform == 'win32':
+    #         self._setup_windows_task(cmd=cmd, task_name=self.name)
+    #     else:
+    #         self._setup_linux_crontab(cmd=cmd)
--- tests/test_bootstrap.py
+++ tests/test_bootstrap.py
@@ -23,26 +23,20 @@ def get_bs(*args, **kwargs):
 
 
 class CrontabTestCase(unittest.TestCase):
-    def _get_bs(self, schedule_minutes):
-        return get_bs(name=NAME,
-                      cmd_args=['main'],
-                      schedule_minutes=schedule_minutes)
+    def setUp(self):
+        self.bs = get_bs(name=NAME, tasks=[{'name': 'test', 'args': ['main']}])
 
     def test_1(self):
-        bs = self._get_bs(schedule_minutes=1)
-        self.assertEqual(bs._generate_crontab_schedule(), '* * * * *')
+        self.assertEqual(self.bs._generate_crontab_schedule(schedule_minutes=1), '* * * * *')
 
     def test_2(self):
-        bs = self._get_bs(schedule_minutes=15)
-        self.assertEqual(bs._generate_crontab_schedule(), '*/15 * * * *')
+        self.assertEqual(self.bs._generate_crontab_schedule(schedule_minutes=15), '*/15 * * * *')
 
     def test_3(self):
-        bs = self._get_bs(schedule_minutes=60 * 2 + 1)
-        self.assertEqual(bs._generate_crontab_schedule(), '0 */2 * * *')
+        self.assertEqual(self.bs._generate_crontab_schedule(schedule_minutes=60 * 2 + 1), '0 */2 * * *')
 
     def test_4(self):
-        bs = self._get_bs(schedule_minutes=24 * 60 + 1)
-        self.assertEqual(bs._generate_crontab_schedule(), '0 0 * * *')
+        self.assertEqual(self.bs._generate_crontab_schedule(schedule_minutes=24 * 60 + 1), '0 0 * * *')
 
 
 class BootstrapperTestCase(unittest.TestCase):
@@ -51,41 +45,28 @@ class BootstrapperTestCase(unittest.TestCase):
         os.makedirs(WORK_DIR)
         self.args = {
             'name': NAME,
-            'cmd_args': ['module.main', 'arg', '--flag'],
+            'tasks': [{'name': 'test', 'args': ['module.main', 'arg', '--flag']}],
         }
         self.bs = get_bs(**self.args)
 
-    def test_cmd(self):
-        bs = get_bs(name=NAME)
-        self.assertRaises(SystemExit, bs._get_cmd)
-
-        bs = get_bs(name=NAME, cmd_args=['module.main'])
-        self.assertEqual(bs._get_cmd()[1:], ['-m', 'module.main'])
-
     def test_attrs(self):
         dirname = f'.{self.args["name"]}'
-        self.assertEqual(self.bs.venv_dir,
-                         os.path.join(os.path.expanduser('~'),
-                                      dirname, module.VENV_DIRNAME))
+        self.assertEqual(self.bs.venv_dir, os.path.join(os.path.expanduser('~'), dirname, module.VENV_DIRNAME))
         bin_dirname = 'Scripts' if sys.platform == 'win32' else 'bin'
         pip_filename = 'pip.exe' if sys.platform == 'win32' else 'pip'
         py_filename = 'pythonw.exe' if sys.platform == 'win32' else 'python'
-        self.assertEqual(self.bs.pip_path,
-                         os.path.join(os.path.expanduser('~'),
-                                      dirname, module.VENV_DIRNAME, bin_dirname, pip_filename))
-        self.assertEqual(self.bs.svc_py_path,
-                         os.path.join(os.path.expanduser('~'),
-                                      dirname, module.VENV_DIRNAME, bin_dirname, py_filename))
+        self.assertEqual(self.bs.pip_path, os.path.join(os.path.expanduser('~'), dirname, module.VENV_DIRNAME, bin_dirname, pip_filename))
+        self.assertEqual(self.bs.svc_py_path, os.path.join(os.path.expanduser('~'), dirname, module.VENV_DIRNAME, bin_dirname, py_filename))
 
     def test_task(self):
-        with patch.object(self.bs, 'setup_venv'), \
-                patch.object(self.bs, '_setup_windows_task') as mock__setup_windows_task, \
-                patch.object(self.bs, '_setup_linux_crontab') as mock__setup_linux_crontab:
-            self.bs.setup_task()
+        args = ['module.main', 'arg', '--flag']
+        with patch.object(self.bs, '_setup_windows_task') as mock__setup_windows_task, \
+             patch.object(self.bs, '_setup_linux_crontab') as mock__setup_linux_crontab:
+            self.bs._setup_task(name='test', args=args)
             if sys.platform == 'win32':
                 cmd = mock__setup_windows_task.call_args_list[0].kwargs['cmd']
             else:
                 cmd = mock__setup_linux_crontab.call_args_list[0].kwargs['cmd']
             cmd = cmd.split(' ')
             print(cmd)
-            self.assertEqual(cmd[1:], ['-m'] + self.args['cmd_args'])
+            self.assertEqual(cmd[1:], ['-m'] + args)
